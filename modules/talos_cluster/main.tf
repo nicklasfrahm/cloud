8e7c66071d43f170110e9d34e4fb3e3b12d192dc
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-03-29 00:29:10 +0100

    fix(ci): create separate opentofu workflow (#42)

---
+++ modules/talos_cluster/main.tf
@@ -0,0 +1,19 @@
+locals {
+  name = var.region.metadata.name
+  talos_version = yamldecode(file("${path.cwd}/config.yaml")).talos.version
+}
+
+# Create the Talos secret bundle.
+resource "talos_machine_secrets" "this" {
+  talos_version = local.talos_version
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+# Create a file with the Talos version.
+# resource "local_file" "version" {
+#   filename = "${path.cwd}/deploy/tofu/out/${local.name}"
+#   content = "${local.talos_version}/${random_id.this.hex}"
+# }
--- modules/talos_cluster/providers.tf
+++ modules/talos_cluster/providers.tf
@@ -10,5 +10,9 @@ terraform {
       source = "siderolabs/talos"
       version = "0.7.1"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.1"
+    }
   }
 }
