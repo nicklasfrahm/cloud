4aaf17ff304eff371567b66a84c6316858095bfa
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-04-23 23:48:52 +0200

    feat(talismand): create PoC server that serves gRPC for KMS and REST API

---
+++ cmd/talismand/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/nicklasfrahm/cloud/pkg/kms"
+	taloskms "github.com/siderolabs/kms-client/api/kms"
+	"github.com/soheilhy/cmux"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+// Dummy gRPC server implementation
+type myGRPCServer struct{}
+
+// func (s *myGRPCServer) SomeMethod(...) { ... }
+
+func main() {
+	// Get the port from the environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	m := cmux.New(listener)
+
+	grpcL := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
+	httpL := m.Match(cmux.Any()) // Now handle both HTTP/1 and h2c in one place
+
+	// Set up REST handlers
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Pong from REST (h2c supported)\n")
+	})
+
+	// Wrap the HTTP handler with h2c support
+	h2s := &http2.Server{}
+	h1h2cHandler := h2c.NewHandler(mux, h2s)
+
+	// gRPC server
+	grpcServer := grpc.NewServer()
+	taloskms.RegisterKMSServiceServer(grpcServer, &kms.KMSServiceServer{})
+
+	// Enable gRPC reflection
+	reflection.Register(grpcServer)
+
+	// Start gRPC
+	go func() {
+		log.Println("Starting gRPC server on :" + port)
+		if err := grpcServer.Serve(grpcL); err != nil {
+			log.Fatalf("gRPC server failed: %v", err)
+		}
+	}()
+
+	// Start REST with h2c support
+	go func() {
+		log.Println("Starting REST server on :" + port)
+		if err := http.Serve(httpL, h1h2cHandler); err != nil {
+			log.Fatalf("REST server failed: %v", err)
+		}
+	}()
+
+	log.Println("cmux multiplexer running on :" + port)
+	if err := m.Serve(); err != nil {
+		log.Fatalf("cmux server failed: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,11 @@ toolchain go1.24.1
 require (
 	github.com/onsi/ginkgo/v2 v2.23.3
 	github.com/onsi/gomega v1.36.3
+	github.com/siderolabs/kms-client v0.1.0
+	github.com/soheilhy/cmux v0.1.5
 	github.com/spf13/cobra v1.9.1
+	golang.org/x/net v0.37.0
+	google.golang.org/grpc v1.65.0
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/apimachinery v0.32.3
 	k8s.io/cli-runtime v0.32.3
@@ -82,7 +86,6 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/oauth2 v0.23.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
@@ -93,7 +96,6 @@ require (
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240826202546-f6391c0de4c7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240826202546-f6391c0de4c7 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
