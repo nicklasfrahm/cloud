448e800a3d0cb83a4cb60b9801dcc76e5360d843
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-06-28 22:41:10 +0200

    fix(tofu): avoid invalid provider

--- deploy/opentofu/providers.tf
+++ deploy/opentofu/providers.tf
@@ -1,6 +1,17 @@
 terraform {
   required_version = ">= 1.9.0"
 
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.8.1"
+    }
+  }
+
   backend "gcs" {
     bucket = "nicklasfrahm"
     prefix = "tofu/state/homelab"
