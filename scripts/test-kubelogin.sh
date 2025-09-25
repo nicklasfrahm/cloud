e3f70f1c6e6c13f126f8806ea860c3bcc037d0c0
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-09-25 21:34:51 +0200

    fix(tools): avoid invalid yaml query

--- scripts/test-kubelogin.sh
+++ scripts/test-kubelogin.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-issuer_url=$(yq .kubernetes.oidc.issuer_url config.yaml)
-client_id=$(yq .kubernetes.oidc.client_id config.yaml)
+issuer_url=$(yq -r .kubernetes.oidc.issuer_url config.yaml)
+client_id=$(yq -r .kubernetes.oidc.client_id config.yaml)
 
 echo -e "Using issuer: \e[1;35m$issuer_url\e[0m"
-kubelogin get-token \
+kubectl oidc-login get-token \
   --oidc-issuer-url="$issuer_url" \
   --oidc-client-id="$client_id" \
-  --oidc-extra-scope="email"
+  --oidc-extra-scope="email" \
+  --oidc-extra-scope="groups" | jq .
