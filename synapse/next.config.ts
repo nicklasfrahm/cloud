6ce6e0f0503f47be5bf8f982d5c7a73c5623d4b2
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-11-02 13:51:15 +0100

    fix(synapse): move to separate repo

--- synapse/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Change to export SPA.
-  output: "standalone",
-  /* config options here */
-};
-
-export default nextConfig;
--- synapse/src/app/api/synapse/services/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { fsClient } from "../../../../lib/filesystemClient";
-
-export async function GET() {
-  const services = await fsClient.listServices();
-  return NextResponse.json({ services });
-}
